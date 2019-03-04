0d7b0e369166d0a7ac5869b820033ebab9d903ad
Vane Salazar <vane_32311@hotmail.com>
2019-03-04 19:31:27 +0100

    cambios

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,76 @@
+<script src="bower_components/firebaseui/dist/firebaseui.js"></script>
+<link type="text/css" rel="stylesheet" href="bower_components/firebaseui/dist/firebaseui.css" />
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        socialapp
-      </h1>
-      <h2 class="subtitle">
-        Social Application IRAV UAM
-      </h2>
-      <AlbumCard :album="album" v-for="album in albums"/>
-    </div>
-  </section>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-xs-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>For more information on Vuetify, check out the <a
+            href="https://vuetifyjs.com"
+            target="_blank"
+          >documentation</a>.</p>
+          <p>If you have questions, please join the official <a
+            href="https://chat.vuetifyjs.com/"
+            target="_blank"
+            title="chat"
+          >discord</a>.</p>
+          <p>Find a bug? Report it on the github <a
+            href="https://github.com/vuetifyjs/vuetify/issues"
+            target="_blank"
+            title="contribute"
+          >issue board</a>.</p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+          >Nuxt Documentation</a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >Nuxt GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            flat
+            nuxt
+            to="/inspire"
+          >Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import axios from 'axios';
-import env from '../config/env'
-import AlbumCard from '../components/AlbumCard'
+
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components:{
-    AlbumCard:AlbumCard
-  },
-  data(){
-    return{
-      albums:[]
-    }
-  },
-  created(){
-    axios.get(`${env.endpoint}/albums`).then(response=>{
-      this.albums = response.data;
-    })
+  components: {
+    Logo,
+    VuetifyLogo
   }
 }
-</script>
-
-<style scoped>
-.container {
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-</style>
-<script src="https://www.gstatic.com/firebasejs/5.8.4/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD13hX-WKrjeFYl9NoMaROsGRpxSbEUONk",
-    authDomain: "socialapp-irav.firebaseapp.com",
-    databaseURL: "https://socialapp-irav.firebaseio.com",
-    projectId: "socialapp-irav",
-    storageBucket: "socialapp-irav.appspot.com",
-    messagingSenderId: "381930007179"
-  };
-  firebase.initializeApp(config);
 </script>
